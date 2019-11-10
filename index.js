3320348466e16223b95065055ee301526f8e3baf
p <pavel.wh@yandex.ru>
2019-11-10 12:56:07 +0300

    Подготовка корзины

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 const homeRoutes = require('./routes/home')
 const coursesRoutes = require('./routes/courses')
 const addRoutes = require('./routes/add')
+const cartRoutes = require('./routes/cart')
 
 const hbs = expressHandlebars.create({
     defaultLayout: 'main',
@@ -21,6 +22,7 @@ app.use(express.urlencoded({extended: true}))
 app.use('/', homeRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/add', addRoutes)
+app.use('/cart', cartRoutes)
 
 const PORT = process.env.PORT || 3000
 
--- models/course.js
+++ models/course.js
@@ -19,28 +19,6 @@ class Course {
         }
     }
 
-    static async update(course) {
-        const courses = await Course.getAll()
-
-        const idx = courses.findIndex(c => c.id === course.id)
-        courses[idx] = course 
-
-        return new Promise((resolve, reject) => {
-            fs.writeFile(
-                path.join(__dirname, '..', 'data', 'courses.json'),
-                JSON.stringify(courses),
-                (error) => {
-                    if (error) {
-                        reject(error)
-                    } else {
-                        resolve()
-                    }
-                }
-            )
-        })
-
-    }
-
     async save() {
         const courses = await Course.getAll()
 
@@ -80,6 +58,28 @@ class Course {
     static async getById(id) {
         const courses = await Course.getAll()
         return courses.find(c => c.id === id)
+    }
+
+    static async update(course) {
+        const courses = await Course.getAll()
+
+        const idx = courses.findIndex(c => c.id === course.id)
+        courses[idx] = course 
+
+        return new Promise((resolve, reject) => {
+            fs.writeFile(
+                path.join(__dirname, '..', 'data', 'courses.json'),
+                JSON.stringify(courses),
+                (error) => {
+                    if (error) {
+                        reject(error)
+                    } else {
+                        resolve()
+                    }
+                }
+            )
+        })
+
     }
 }
 
