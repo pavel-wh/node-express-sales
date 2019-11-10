f8a4c1e57a6d90e75d3945b8717a4cb305492980
p <pavel.wh@yandex.ru>
2019-11-10 08:08:14 +0300

    Работа с HTML-файлами

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200)
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+app.get('/about', (req, res) => {
+    res.status(200)
+    res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(3000, () => {
+    console.log(`Server is running on port ${ PORT }`)
+})
