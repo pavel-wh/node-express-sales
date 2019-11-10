0c0019dff1d2499e9411ef8969fa157570a987a5
p <pavel.wh@yandex.ru>
2019-11-10 14:05:00 +0300

    Динамическое изменение корзины

--- public/app.js
+++ public/app.js
@@ -1,8 +1,12 @@
-document.querySelectorAll('.price').forEach(node => {
-    node.textContent = new Intl.NumberFormat('ru-RU', {
+const toCurrency = price => {
+    return new Intl.NumberFormat('ru-RU', {
         currency: 'rub',
         style: 'currency'
-    }).format(node.textContent)
+    }).format(price)
+}
+
+document.querySelectorAll('.price').forEach(node => {
+    node.textContent = toCurrency(node.textContent)
 })
 
 const $cart = document.querySelector("#cart")
@@ -15,7 +19,23 @@ if ($cart) {
                 method: 'delete'
             }).then(res => res.json())
               .then(cart => {
-                  console.log(cart)
+                  if (cart.courses.length) {
+                    const html = cart.courses.map(c => {
+                        return `
+                            <tr>
+                                <td>${ c.title }</td>
+                                <td>${ c.count }</td>
+                                <td>
+                                    <button class="btn btn-small js-remove" data-id="${ c.id }">Удалить</button>
+                                </td>
+                            </tr>
+                        `
+                    }).join('')
+                    $cart.querySelector('tbody').innerHTML = html
+                    $cart.querySelector('.price').innerText = toCurrency(cart.price)
+                  } else {
+                      $cart.innerHTML = '<p>Корзина пуста</p>'
+                  }
               })
         }
     })
