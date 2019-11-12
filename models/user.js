1d33ec8cf2bb40325ed598165116046ed53c3198
p <pavel.wh@yandex.ru>
2019-11-12 04:13:42 +0300

    Регистрация пользователя

--- models/user.js
+++ models/user.js
@@ -5,7 +5,8 @@ const userSchema = new Schema ({
         type: String,
         require: true
     },
-    name: {
+    name: String,
+    password: {
         type: String,
         required: true
     },
--- routes/auth.js
+++ routes/auth.js
@@ -27,4 +27,24 @@ router.get('/logout', async  (req, res) => {
     })
 })
 
+router.post('/registration', async  (req, res) => {
+    try {
+        const { name, email, password, confirm } = req.body
+        const candidate = await User.findOne({ email })
+
+        if (candidate) {
+            res.redirect('/auth/login#registration')
+        } else {
+            const user = new User({
+                name, email, password, cart: {items: []}
+            })
+            await user.save()
+            res.redirect('/auth/login#login')
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
 module.exports = router
