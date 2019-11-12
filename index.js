fc7067ea963970767e62dae777da98d4f818f96c
p <pavel.wh@yandex.ru>
2019-11-12 03:13:59 +0300

    Сохранение сессии

--- index.js
+++ index.js
@@ -23,16 +23,6 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
-app.use(async (req, res, next) => {
-    try {
-        const user = await User.findById('5dc81235617a63284f984ce8')
-        req.user = user
-        next()
-    } catch (error) {
-        console.log(error)
-    }
-})
-
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 app.use(session({
@@ -70,17 +60,17 @@ async function start() {
         }
         
         await mongoose.connect(MONGO_URI, options)    
-        const candidate = await User.findOne()
-        if (!candidate) {
-            const user = new User({
-                email: 'pavel.wh@yandex.ru',
-                name: 'pavel',
-                cart: {
-                    items: []
-                }
-            })
-            await user.save()
-        }
+        // const candidate = await User.findOne()
+        // if (!candidate) {
+        //     const user = new User({
+        //         email: 'pavel.wh@yandex.ru',
+        //         name: 'pavel',
+        //         cart: {
+        //             items: []
+        //         }
+        //     })
+        //     await user.save()
+        // }
         app.listen(PORT, () => {
             console.log(`Server is running on port ${ PORT }`)
         })
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const { Router } = require('express')
+const User = require('../models/user')
 const router = Router()
 
 router.get('/login', async  (req, res) => {
@@ -9,8 +10,15 @@ router.get('/login', async  (req, res) => {
 })
 
 router.post('/login', async  (req, res) => {
+    const user = await User.findById('5dc81235617a63284f984ce8')
+    req.session.user = user
     req.session.isAuthenticated = true
-    res.redirect('/')
+    req.session.save(error => {
+        if (error) {
+            throw error
+        }
+        res.redirect('/')
+    })
 })
 
 router.get('/logout', async  (req, res) => {
