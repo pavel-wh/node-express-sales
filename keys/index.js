768b3e3d4c5bc2b697ea93805dadfd16c8cbcc30
p <pavel.wh@yandex.ru>
2019-11-12 06:43:43 +0300

    Настройка почтового сервиса

--- keys/index.js
+++ keys/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     MONGO_URI: 'mongodb+srv://pavel:kV3F5186uZhuLpRY@cluster0-pptdb.mongodb.net/test?retryWrites=true&w=majority',
-    SESSION_SECRET: 'some secret value'
+    SESSION_SECRET: 'some secret value',
+    SENDGRID_API_KEY: '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'
 }
