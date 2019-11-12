1c1ebd4649882e4f09e9656432947932bc196085
p <pavel.wh@yandex.ru>
2019-11-12 08:32:21 +0300

    Восстановление пароля

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,12 @@
 const { Router } = require('express')
 const bcrypt = require('bcryptjs')
+const crypto = require('crypto')
 const nodemailer = require('nodemailer')
 const sendgrid = require('nodemailer-sendgrid-transport')
 const User = require('../models/user')
 const keys = require('../keys')
 const regEmail = require('../emails/registration')
+const resetEmail = require('../emails/reset')
 const router = Router()
 
 const transporter = nodemailer.createTransport(sendgrid({
@@ -77,6 +79,40 @@ router.post('/registration', async  (req, res) => {
             await transporter.sendMail(regEmail(email))
         }
 
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.get('/reset', (req, res) => {
+    res.render('auth/reset', {
+        title: 'Забыли пароль?',
+        error: req.flash('error')
+    })
+})
+
+router.post('/reset', (req, res) => {
+    try {
+        crypto.randomBytes(32, async (error, buffer) => {
+            if (error) {
+                req.flash('error', 'Что-то пошло не так, повторите попытку позже')
+                res.redirect('/auth/reset')
+            }
+            const token = buffer.toString('hex')
+            const candidate = User.findOne({
+                email: req.body.email
+            })
+
+            if (candidate) {
+                candidate.resetToken = token,
+                candidate.resetTokenExp = Date.now() + 60 * 60 * 1000
+                await candidate.save()
+                await transporter.sendMail(resetEmail(candidate.email, token))
+                res.redirect('/auth/login')
+            } else {
+                req.flash('error', 'Такого email нет')
+            }
+        })
     } catch (error) {
         console.log(error)
     }
