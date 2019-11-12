c84a201e7d58510253da264e4afb92844fe41df8
p <pavel.wh@yandex.ru>
2019-11-12 09:32:10 +0300

    Изменение пароля

--- routes/auth.js
+++ routes/auth.js
@@ -91,32 +91,6 @@ router.get('/reset', (req, res) => {
     })
 })
 
-router.get('/password/:token', async (req, res) => {
-    if (!req.params.token) {
-        return res.redirect('/auth/login')
-    } 
-    try {
-        const user = await User.findOne({
-            resetToken: req.params.token,
-            resetTokenExp: { $gt: Date.now() }
-        })
-    
-        if(!user) {
-            return res.redirect('/auth/login')
-        } else {
-            res.render('auth/reset', {
-                title: 'Восстановить доступ',
-                error: req.flash('error'),
-                userId: user._id.toString(),
-                token: req.params.token
-            })
-        }
-
-    } catch (error) {
-        console.log(error)
-    }
-})
-
 router.post('/reset', (req, res) => {
     try {
         crypto.randomBytes(32, async (error, buffer) => {
@@ -140,6 +114,56 @@ router.post('/reset', (req, res) => {
                 res.redirect('/auth/reset')
             }
         })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.post('/password', async (req, res) => {
+    try {
+        const user = await User.findOne({
+            _id: req.body.userId,
+            resetToken: req.body.token,
+            resetTokenExp: {$gt: Date.now()}
+        })
+
+        if (user) {
+            user.password = await bcrypt.hash(req.body.password, 12)
+            user.resetToken = undefined
+            user.resetTokenExp = undefined
+            await user.save()
+            res.redirect('/auth/login')
+        } else {
+            req.flash('loginError', 'Время жизни токена истекло')
+            res.redirect('/auth/login')
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.get('/password/:token', async (req, res) => {
+    if (!req.params.token) {
+        return res.redirect('/auth/login')
+    } 
+    try {
+        const user = await User.findOne({
+            resetToken: req.params.token,
+            resetTokenExp: { $gt: Date.now() }
+        })
+    
+        if(!user) {
+            return res.redirect('/auth/login')
+        } else {
+            res.render('auth/password', {
+                title: 'Восстановить доступ',
+                error: req.flash('error'),
+                userId: user._id.toString(),
+                token: req.params.token
+            })
+        }
+
     } catch (error) {
         console.log(error)
     }
