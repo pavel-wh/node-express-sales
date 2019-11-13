f96a775df9d4b2828fdb945a680aabbaa82fe467
p <pavel.wh@yandex.ru>
2019-11-13 07:44:01 +0300

    Защита ключей

--- keys/index.js
+++ keys/index.js
@@ -1,7 +1,5 @@
-module.exports = {
-    MONGO_URI: 'mongodb+srv://pavel:kV3F5186uZhuLpRY@cluster0-pptdb.mongodb.net/test?retryWrites=true&w=majority',
-    SESSION_SECRET: 'some secret value',
-    SENDGRID_API_KEY: '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',
-    EMAIL_FROM: 'node-express-shop@test.ru',
-    BASE_URL: 'http://localhost:3000'
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys.prod')
+} else {
+    module.exports = require('./keys.dev')
 }
