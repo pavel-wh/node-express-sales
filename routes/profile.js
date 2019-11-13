3a6ebee2a01fba22d5bd8273afe92b9e57ac0eab
p <pavel.wh@yandex.ru>
2019-11-13 06:06:13 +0300

    Создание страницы профиля

---
+++ routes/profile.js
@@ -0,0 +1,17 @@
+const { Router } = require('express')
+const auth = require('../middleware/auth')
+const router = Router()
+
+router.get('/', auth, async (req, res) => {
+    res.render('profile', {
+        title: 'Профиль',
+        isProfile: true,
+        user: req.user.toObject()
+    })
+})
+
+router.post('/', auth, async (req, res) => {
+    
+})
+
+module.exports = router
